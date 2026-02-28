4b1f7a27e97fa299418d0c59e4059fa98ec37262
Fraaail <74563111+Fraaail@users.noreply.github.com>
2026-02-28 19:15:32 +0800

    Use GOOGLE_MAPS_LINK for map iframe
    Replace the hardcoded Google Maps embed URL in components/Location.tsx with the GOOGLE_MAPS_LINK constant to centralize the map source and make updates/configuration easier. Ensure GOOGLE_MAPS_LINK is defined or imported where this component is used.

--- components/Location.tsx
+++ components/Location.tsx
@@ -62,7 +62,7 @@ export const Location: React.FC = () => {
           <div className="relative h-96 w-full rounded-3xl overflow-hidden border-4 border-brand-red/30 shadow-2xl group">
             {/* Embedded Google Map focusing on Nagcarlan/Banago area */}
             <iframe 
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3868.583492716309!2d121.4137254!3d14.1352469!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33bd5bd27fa98719%3A0x280f555193910543!2sDace's%20Samgyeopsal!5e0!3m2!1sen!2sph!4v1709904221532!5m2!1sen!2sph" 
+              src={GOOGLE_MAPS_LINK}
               width="100%" 
               height="100%" 
               style={{ border: 0 }} 
